b21336d15d45417b127593a6c1b04ba88e90baac
evengers <steven.rogers@evengers.com>
2023-12-18 14:05:06 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-// Contents of script.js
-alert("Hello from external script!");
+// Contents of script.
+// Step 1: Check local storage and display the result
+var storedValue = localStorage.getItem('yourKey');
+alert('Stored Value: ' + storedValue);
+
+// Step 2: Prompt user to change the value
+var userInput = prompt('Enter a new value:', storedValue);
+
+if (userInput !== null) {
+    // Step 3: Store the change if any
+    localStorage.setItem('yourKey', userInput);
+    alert('Value changed to: ' + userInput);
+
+    // Step 4: Retain the value in a variable for use elsewhere
+    storedValue = userInput;
+}
+
+// Now you can use the 'storedValue' variable elsewhere in your code.
